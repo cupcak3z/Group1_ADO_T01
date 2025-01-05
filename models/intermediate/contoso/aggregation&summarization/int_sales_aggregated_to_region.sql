847af2590d9cf8516a366aa5a3f9467a8d207aea
Zavier Tan <S10255651@connect.np.edu.sg>
2025-01-05 08:34:47 +0000

    done the intermediate model for sales to region data

--- models/intermediate/contoso/aggregation&summarization/int_sales_aggregated_to_region.sql
+++ models/intermediate/contoso/aggregation&summarization/int_sales_aggregated_to_region.sql
@@ -1,33 +1,19 @@
 with
-
-sales as (
-    select * from {{ ref('stg_contoso__sales') }}
-),
-store as (
-    select * from {{ ref('stg_contoso__store') }}
-),
-geography as (
-    select * from {{ ref('stg_contoso__geography') }}
-),
-
 salesregiondata as (
-    select
-        s.SALESKEY_UPDATED,
-        s.STOREKEY_UPDATED,
-        s.DATEKEY_UPDATED,
-        s.PRODUCTKEY_UPDATED,
-        s.SALESAMOUNT_UPDATED,
-        s.NET_SALES_AMOUNT,
-        s.TOTAL_PROFIT,
-        st.STORETYPE,
-        st.STORENAME,
-        st.STATUS,
-        g.REGIONCOUNTRYNAME
-    from sales s
-    left join store st on s.STOREKEY_UPDATED = st.STOREKEY_UPDATED
-    left join geography g on st.GEOGRAPHYKEY_UPDATED = g.GEOGRAPHYKEY_UPDATED
+    select * from {{ ref('int_sales_joinregiondata') }}  -- Referencing the join model
 )
 
-select * from salesregiondata
+-- Aggregate sales by Country and Store Type
+select
+    REGIONCOUNTRYNAME, 
+    STORETYPE,
+    round(sum(SALESAMOUNT_UPDATED), 2) as total_sales_amount,
+    round(sum(NET_SALES_AMOUNT), 2) as total_net_sales,
+    round(sum(TOTAL_PROFIT), 2) as total_profit
+from salesregiondata
+group by REGIONCOUNTRYNAME, STORETYPE
+
+
+
 
 
