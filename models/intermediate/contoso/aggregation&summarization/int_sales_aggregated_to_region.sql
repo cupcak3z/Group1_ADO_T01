02ccb159eeb3d63e939271fefc9c80ab6e0a11bd
Zavier Tan <S10255651@connect.np.edu.sg>
2025-01-05 07:37:45 +0000

    .

--- models/intermediate/contoso/aggregation&summarization/int_sales_aggregated_to_region.sql
+++ models/intermediate/contoso/aggregation&summarization/int_sales_aggregated_to_region.sql
@@ -1,3 +1,5 @@
+with
+
 sales as (
     select * from {{ ref('stg_contoso__sales') }}
 ),
@@ -7,15 +9,25 @@ store as (
 geography as (
     select * from {{ ref('stg_contoso__geography') }}
 ),
+
 salesregiondata as (
     select
-        s.*,
-        st.*,
-        g.*
+        s.SALESKEY_UPDATED,
+        s.STOREKEY_UPDATED,
+        s.DATEKEY_UPDATED,
+        s.PRODUCTKEY_UPDATED,
+        s.SALESAMOUNT_UPDATED,
+        s.NET_SALES_AMOUNT,
+        s.TOTAL_PROFIT,
+        st.STORETYPE,
+        st.STORENAME,
+        st.STATUS,
+        g.REGIONCOUNTRYNAME
     from sales s
-    left join store st on s.STOREKEY_updated = st.STOREKEY_updated
-    left join geography g on st.GEOGRAPHYKEY_updated = g.GEOGRAPHYKEY_updated
+    left join store st on s.STOREKEY_UPDATED = st.STOREKEY_UPDATED
+    left join geography g on st.GEOGRAPHYKEY_UPDATED = g.GEOGRAPHYKEY_UPDATED
 )
 
 select * from salesregiondata
+
 
