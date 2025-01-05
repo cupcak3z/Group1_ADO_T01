cee3e0739f9e82484fab929c0bb8589e4a29152c
Zavier Tan <S10255651@connect.np.edu.sg>
2025-01-05 09:24:24 +0000

    Did intermediate model for profitability but to be refined.

--- models/intermediate/contoso/joinsrelationships/int_sales_joinproductgeography.sql
+++ models/intermediate/contoso/joinsrelationships/int_sales_joinproductgeography.sql
@@ -1,28 +1,29 @@
 with
--- FactSales table (sales data)
+
 sales as (
-    select * from {{ ref('stg_contoso__sales') }}  -- Sales data from FactSales
+    select * from {{ ref('stg_contoso__sales') }}
 ),
--- DimProduct table (product details)
+
 product as (
-    select * from {{ ref('stg_contoso__product') }}  -- Product data from DimProduct
+    select * from {{ ref('stg_contoso__product') }}
 ),
--- DimGeography table (geography details)
+
 geography as (
-    select * from {{ ref('stg_contoso__geography') }}  -- Geography data from DimGeography
+    select * from {{ ref('stg_contoso__geography') }}
 ),
 
 -- Join sales, product, and geography data
 int_sales_product_profitability as (
     select
-        p.PRODUCTNAME,  -- Product name from DimProduct
-        p.CLASSNAME,  -- Product class from DimProduct
-        g.REGIONCOUNTRYNAME,  -- Region from DimGeography
-        s.SALESAMOUNT_UPDATED,  -- Sales amount from FactSales
-        s.SALESQUANTITY_UPDATED  -- Sales quantity from FactSales
+        p.PRODUCTNAME, 
+        p.CLASSNAME, 
+        g.REGIONCOUNTRYNAME,
+        s.SALESAMOUNT_UPDATED, 
+        p.UNITCOST_updated,
+        s.SALESQUANTITY_UPDATED 
     from sales s
-    left join product p on s.PRODUCTKEY_UPDATED = p.PRODUCTKEY_UPDATED  -- Join to get product data
-    left join geography g on s.STOREKEY_UPDATED = g.GEOGRAPHYKEY_UPDATED  -- Join to get geography data
+    left join product p on s.PRODUCTKEY_UPDATED = p.PRODUCTKEY_UPDATED 
+    left join geography g on s.STOREKEY_UPDATED = g.GEOGRAPHYKEY_UPDATED
 )
 
 select * from int_sales_product_profitability
