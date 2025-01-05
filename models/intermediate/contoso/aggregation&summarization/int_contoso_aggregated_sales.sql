7f743ffe0b753841d6507a0cad33527b9add372a
Arjunan Yohith <s10259990@connect.np.edu.sg>
2025-01-05 09:08:19 +0000

    Created Intermediate Table to see trends across months

--- models/intermediate/contoso/aggregation&summarization/int_contoso_aggregated_sales.sql
+++ models/intermediate/contoso/aggregation&summarization/int_contoso_aggregated_sales.sql
@@ -1,24 +1,23 @@
 with 
-
 sales as (
-    select * from {{ ref('stg_contoso_sales') }}  
+    select * 
+    from {{ ref('stg_contoso__sales') }}
 ),
 
 aggregated_sales as (
     select
-        PRODUCTKEY_updated,
-        STOREKEY_updated,        
         date_trunc('month', created_at) as month,
-        NET_SALES_AMOUNT,
-        TOTAL_PROFIT,
         sum(SALESAMOUNT_updated) as total_sales,
         avg(SALESAMOUNT_updated) as avg_sales,
         sum(SALESQUANTITY_updated) as total_quantity,
-        avg(SALESQUANTITY_updated) as avg_quantity
+        avg(SALESQUANTITY_updated) as avg_quantity,
+        sum(NET_SALES_AMOUNT) as total_net_sales,
+        sum(TOTAL_PROFIT) as total_profit
     from sales
-    group by 1, 2, 3, 4, 5
+    group by 
+        date_trunc('month', created_at)
 )
 
 select *
 from aggregated_sales
-order by PRODUCTKEY_updated, STOREKEY_updated
+order by month
