ef449a821cd84b885e3ea7869591da1fb90d37ac
Hong Kai Tan <Hongkaihktan@gmail.com>
2025-01-06 02:39:06 +0000

    Finish Derived Metrics

--- models/intermediate/contoso/derivedmetrics/int_promotion_datecalculation.sql
+++ models/intermediate/contoso/derivedmetrics/int_promotion_datecalculation.sql
@@ -15,7 +15,7 @@ promotion_derivedmetrics as (
 
         -- numbers
         DISCOUNTPERCENT_UPDATED,
-        datediff('day', STARTDATE_UPDATED, ENDDATE_UPDATED) as PROMOTIONDURATION,
+        datediff('day', STARTDATE_UPDATED, ENDDATE_UPDATED) as DAYSPROMOTIONDURATION,
         case
             when cast('2009-10-01' as date) < STARTDATE_UPDATED then 
                 -datediff('day', cast('2009-10-01' as date), STARTDATE_UPDATED)
