2893a412feec0cbde8b2566046e21adfcdfa050e
Louis Yong <louisyong1468@gmail.com>
2025-01-07 13:41:45 +0000

    udpated centralised join table

--- models/intermediate/contoso/completedfullsales_table.sql
+++ models/intermediate/contoso/completedfullsales_table.sql
@@ -2,60 +2,136 @@ with
 salesdata as (
     select
         SALESKEY_updated,
-
+        DATEKEY_updated,
+        STOREKEY_updated,
+        PRODUCTKEY_updated,
+        PROMOTIONKEY_updated,
+        CHANNELKEY_updated,
+        CURRENCYKEY_updated,
+        SALESAMOUNT_updated,
+        DISCOUNTAMOUNT_updated,
+        RETURNAMOUNT_updated,
+        SALESQUANTITY_updated,
+        DISCOUNTQUANTITY_updated,
+        RETURNQUANTITY_updated,
+        UNITCOST_updated,
+        UNITPRICE_updated,
+        TOTALCOST_updated,
+        NET_SALES_AMOUNT,
+        TOTAL_PROFIT
     from {{ ref('stg_contoso__sales') }}
 ),
 
 datedata as (
     select
         DATEKEY_updated,
-        
+        CALENDARYEAR_updated,
+        CALENDARMONTHLABEL,
+        CALENDARHALFYEARLABEL,
+        CALENDARQUARTERLABEL,
+        CALENDARWEEKLABEL,
+        CALENDARDAYOFWEEKLABEL,
+        FISCALYEAR_updated,
+        FISCALHALFYEARLABEL,
+        FISCALQUARTERLABEL,
+        FISCALMONTHLABEL,
+        ISHOLIDAY_updated,
+        ISWORKDAY,
+        EUROPESEASON_updated,
+        NORTHAMERICASEASON_updated,
+        ASIASEASON_updated
+    from {{ ref('stg_contoso__date') }}
 ),
 
 geographydata as (
     select
-        d
+        GEOGRAPHYKEY_updated,
+        GEOGRAPHYTYPE,
+        CONTINENTNAME,
+        CITYNAME,
+        STATEPROVINCENAME,
+        REGIONCOUNTRYNAME
     from {{ ref('stg_contoso__geography') }}
 ),
 
 storedata as (
     select
-        d
+        STOREKEY_updated,
+        GEOGRAPHYKEY_updated,
+        STORETYPE,
+        STORENAME,
+        STATUS,
+        CLOSEREASON_UPDATED,
+        STOREMANAGER_UPDATED,
+        EMPLOYEECOUNT_UPDATED,
+        SELLINGAREASIZE_UPDATED,
+        YEARSSINCEOPEN,
+        YEARSLEFT,
+        DAYSSINCELASTREMODEL
     from {{ ref('stg_contoso__store') }}
 ),
 
 productdata as (
     select
-        d
+        PRODUCTKEY_updated,
+        PRODUCTNAME,
+        PRODUCTCATEGORYNAME,
+        PRODUCTSUBCATEGORYNAME,
+        MANUFACTURER,
+        BRANDNAME,
+        CLASSNAME,
+        COLORNAME,
+        STOCKTYPENAME,
+        UNITCOST_updated,
+        UNITPRICE_updated,
+        MARKUP,
+        DAYSSINCEAVAILABLEFORSALE
     from {{ ref('int_product_jointogether') }}
-),
-
-customerdata as (
-    select
-        d
-    from {{ ref('stg_contoso__customer')}}
 ),
 
 promotiondata as (
     select
-        d
+        PROMOTIONKEY_updated,
+        PROMOTIONNAME,
+        PROMOTIONTYPE,
+        PROMOTIONCATEGORY,
+        DISCOUNTPERCENT_updated,
+        DAYSPROMOTIONDURATION
     from {{ ref('stg_contoso__promotion')}}
 ),
 
 channeldata as (
     select
-        d
+        CHANNELKEY_updated,
+        CHANNELNAME
     from {{ ref('stg_contoso__channel') }}
 ),
 
 currencydata as (
     select
-        d
+        CURRENCYKEY_updated,
+        CURRENCYNAME
     from {{ ref('stg_contoso__currency') }}
 ),
 
 completedsalesdata as (
     select
-        d
-    from 
+        sales.*,
+        dates.*,
+        geog.*,
+        store.*,
+        prod.*,
+        promo.*,
+        chan.*,
+        curr.*
+    from salesdata sales
+    left join datedata dates on sales.DATEKEY_updated = dates.DATEKEY_updated
+    left join storedata store on sales.STOREKEY_updated = store.STOREKEY_updated
+    left join geographydata geog on store.GEOGRAPHYKEY_updated = geog.GEOGRAPHYKEY_updated
+    left join productdata prod on sales.PRODUCTKEY_updated = prod.PRODUCTKEY_updated
+    left join promotiondata promo on sales.PROMOTIONKEY_updated = promo.PROMOTIONKEY_updated
+    left join channeldata chan on sales.CHANNELKEY_updated = chan.CHANNELKEY_updated
+    left join currencydata curr on sales.CURRENCYKEY_updated = curr.CURRENCYKEY_updated
 )
+
+select * from completedsalesdata
